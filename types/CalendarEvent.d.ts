54c8c007eb2cd54f4ff4e39e8f861efc3948e212
Luis Langa <luis.langajr96@gmail.com>
2025-02-15 20:49:41 -0300

    feat(types): event type

---
+++ types/CalendarEvent.d.ts
@@ -0,0 +1,8 @@
+export type CalendarEvent = {
+  calendarEventId: string;
+  userId: string;
+  calendarEventDescription: string;
+  startDate: string;
+  endDate: string;
+  hasGuests: boolean;
+};
