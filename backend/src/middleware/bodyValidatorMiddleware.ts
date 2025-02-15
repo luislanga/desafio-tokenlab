a5ffa10abbfef2a32bb4d90b1b78542f0548f65b
Luis Langa <luis.langajr96@gmail.com>
2025-02-15 19:24:52 -0300

    feat(middlewares): body validator middleware

---
+++ backend/src/middleware/bodyValidatorMiddleware.ts
@@ -0,0 +1,21 @@
+import { Schema } from "joi";
+import { createHttpError } from "../utils/createHttpError";
+
+export const bodyValidatorMiddleware = (schema: Schema) => ({
+  before: (handler: any, next: any) => {
+    const { body } = handler.event;
+    if (!body) {
+      throw createHttpError(400, "Empty body request");
+    }
+
+    const data = JSON.parse(body);
+    const { error } = schema.validate(data, { abortEarly: false });
+
+    if (error) {
+      const message = error.details.map((detail) => detail.message).join(", ");
+      throw createHttpError(400, `Validation Error: ${message}`);
+    }
+
+    return next();
+  },
+});
