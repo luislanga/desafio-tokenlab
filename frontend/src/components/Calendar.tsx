65a18a0a7168e1413e19aff2e60e17e93cd6a4c6
luislanga <luis.langajr96@gmail.com>
2025-02-18 09:53:39 -0300

    more terrible css

--- frontend/src/components/Calendar.tsx
+++ frontend/src/components/Calendar.tsx
@@ -1,13 +1,31 @@
-import { Calendar as BigCalendar, momentLocalizer } from "react-big-calendar";
-import moment from "moment";
+import {
+  Calendar as BigCalendar,
+  dateFnsLocalizer,
+} from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
 import CreateEventModal from "./CreateEventModal";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { useFetchEvents } from "../hooks/useEvents";
 import DeleteEventModal from "./DeleteEventModal";
 import UpdateEventModal from "./UpdateEventModal";
+import { ptBR } from "date-fns/locale/pt-BR";
+import { format } from "date-fns/format";
+import { parse } from "date-fns/parse";
+import { startOfWeek } from "date-fns/startOfWeek";
+import { getDay } from "date-fns/getDay";
 
-const localizer = momentLocalizer(moment);
+
+const locales = {
+  ptBR: ptBR,
+};
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+});
 
 const Calendar = () => {
   const { data: events, isLoading, error } = useFetchEvents();
@@ -27,6 +45,24 @@ const Calendar = () => {
     setSelectedEventInfo(eventInfo);
   };
 
+  const { defaultDate, messages } = useMemo(
+    () => ({
+      defaultDate: new Date(2015, 3, 13),
+      messages: {
+        week: "Semana",
+        work_week: "Semana de trabalho",
+        day: "Dia",
+        month: "Mês",
+        previous: "<",
+        next: ">",
+        today: "Hoje",
+        agenda: "Agenda",
+
+        showMore: (total: any) => `+${total} mais`,
+      },
+    }),
+    []
+  );
   return (
     <div className="container mt-4">
       {createEventModalIsOpen && (
@@ -54,9 +90,11 @@ const Calendar = () => {
       ) : (
         <BigCalendar
           localizer={localizer}
+          culture="ptBR"
+          messages={messages}
           events={events}
           style={{ height: "80vh" }}
-          views={["month", "week", "day"]}
+          views={["month", "day"]}
           onSelectSlot={handleOpenCreateEventModal}
           onSelectEvent={handleOpenDeleteEventModal}
           selectable
