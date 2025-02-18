c6d5233c861d5f770a5f3b607e770ef7188efd97
luislanga <luis.langajr96@gmail.com>
2025-02-18 15:29:55 -0300

    fix(modal): mui font

--- frontend/src/components/CreateEventModal/mui-styles.ts
+++ frontend/src/components/CreateEventModal/mui-styles.ts
@@ -1,21 +1,18 @@
 import { theme } from "../../styles/theme";
 
 export const dateTimePickerStyles = {
-  "& *": {
-    fontFamily: theme.fonts.main,
+  "& .MuiInputLabel-root": {
+    fontFamily: theme.fonts.main || "Inter",
     fontSize: "14px",
     fontWeight: 400,
+    color: theme.colors.secondary,
   },
   "& .MuiInputBase-root": {
     backgroundColor: theme.colors.input_bg,
     borderRadius: "12px",
-    fontFamily: "Inter",
   },
   "& .MuiOutlinedInput-notchedOutline": {
     borderColor: theme.colors.secondary,
-  },
-  "& .MuiInputLabel-root": {
-    color: theme.colors.secondary,
   },
   "&:hover .MuiOutlinedInput-notchedOutline": {
     borderColor: theme.colors.secondary,
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -2,7 +2,6 @@ import Header from "../components/Header/Header";
 import { PageTitle } from "../components/PageTitle/PageTitle";
 import { WidthContainer } from "../components/WidthContainer/WidthContainer";
 import { CreateEventModal } from "../components/CreateEventModal/CreateEventModal";
-import { GenericModal } from "../components/GenericModal/GenericModal";
 
 export const Dashboard = () => {
   return (
