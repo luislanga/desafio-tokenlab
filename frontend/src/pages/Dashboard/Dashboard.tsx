16c1347f3705096c21794201e612d1bc5ec285d0
luislanga <luis.langajr96@gmail.com>
2025-02-20 13:35:14 -0300

    fix(dashboard): margins

--- frontend/src/pages/Dashboard/Dashboard.tsx
+++ frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,14 +1,17 @@
 import { Calendar } from "../../components/Calendar/Calendar";
 import { Header } from "../../components/Header/Header";
 import { WidthContainer } from "../../components/WidthContainer/WidthContainer";
+import { Container } from "./styles";
 
 export const Dashboard = () => {
   return (
     <div>
       <Header />
-      <WidthContainer>
-        <Calendar />
-      </WidthContainer>
+      <Container>
+        <WidthContainer>
+          <Calendar />
+        </WidthContainer>
+      </Container>
     </div>
   );
 };
--- frontend/src/pages/Dashboard/styles.ts
+++ frontend/src/pages/Dashboard/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-
-`;  
+    margin: 36px 0 24px;;
+`;
