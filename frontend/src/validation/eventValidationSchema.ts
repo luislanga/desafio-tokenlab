3b8801de4f40255163d9736f7c054929981a6b95
luislanga <luis.langajr96@gmail.com>
2025-02-20 08:03:16 -0300

    fix(validation): min max string size

--- frontend/src/validation/eventValidationSchema.ts
+++ frontend/src/validation/eventValidationSchema.ts
@@ -1,8 +1,10 @@
 import Joi from "joi";
 
 export const eventValidationSchema = Joi.object({
-  title: Joi.string().required().messages({
+  title: Joi.string().min(1).max(50).required().messages({
     "string.empty": "Descrição obrigatória",
+    "string.min": "Descrição deve ter pelo menos 1 caractere",
+    "string.max": "Descrição deve ter no máximo 50 caracteres",
   }),
   start: Joi.date().iso().required().messages({
     "date.base": "Data de início é inválida",
